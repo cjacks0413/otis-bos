83a377efa633599c14e89ba68c3847630e773c27
cjacks0413 <chjackson93@gmail.com>
2013-08-12 11:20:52 -0400

    initialize rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bpr::Application.config.secret_key_base = 'ac58bbbd0414095ff1778cb22b85952b57cdb1c06c2096e5689f84c2aaf47db222da48efe2487b295f4f91411b08faff52eb5cf376e36b7f682ace116dce19a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bpr::Application.config.session_store :cookie_store, key: '_bpr_session'
