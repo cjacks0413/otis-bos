e3e5e7bf1b603bac67a1fe4535769384adbfee6f
cjacks0413 <chjackson93@gmail.com>
2013-08-14 14:25:41 -0400

    update gitignore

--- app/controllers/restrooms_controller.rb
+++ app/controllers/restrooms_controller.rb
@@ -1,10 +1,29 @@
 class RestroomsController < ApplicationController
   def new
+    @restroom = Restroom.new 
   end
+  
+  def create
+    @restroom = Restroom.new(restroom_params)
+    if @restroom.save
+     redirect_to @restroom
+    else
+      render 'new'
+    end 
+  end 
   
   def index
   end
+  
   def show
     @restroom = Restroom.find(params[:id]) 
   end
+  
+  private
+    
+    def restroom_params
+      params.require(:restroom).permit(:name, :line1, :line2, :city, :state, :zip)
+      
+    end
+    
 end
--- spec/requests/restroom_pages_spec.rb
+++ spec/requests/restroom_pages_spec.rb
@@ -7,21 +7,41 @@ describe "RestroomPages" do
   describe "add restroom page" do
     before { visit new_restroom_path} 
     
-    it { should have_content('Add Restroom') }    
-  end
-  
-  describe "show all restrooms" do 
-    before { visit restrooms_path }    
-    it { should have_content('All Restrooms') } 
-  end
-  
-  describe "restroom page" do 
-    let(:restroom) { FactoryGirl.create(:restroom) }
-    before { visit restroom_path(restroom) } 
+    let(:submit) { "Add This Restroom" } 
     
-    it { should have_content(restroom.name) }
-    it { should have_content(restroom.line1) } 
-  end
+    describe "with invalid information" do
+      it "should not create a restroom" do
+        expect { click_button submit }.not_to change(Restroom, :count)
+      end
+    end
+    
+    describe "with valid information" do 
+	  before do
+		fill_in "restroom_name", with: "Example B.room"
+		fill_in "restroom_line1", with: "112 Alexandria Lane" 
+		fill_in "restroom_city", with: "Boston" 
+		fill_in "restroom_state", with: "MA"
+		fill_in "restroom_zip", with: "02116" 
+	  end
+	
+	  it "should create a restroom" do
+		expect { click_button submit }.to change(Restroom, :count).by(1)
+	  end 
+   end
   
+  
+	describe "show all restrooms" do 
+	  before { visit restrooms_path }    
+	  it { should have_content('All Restrooms') } 
+	end
+  
+	describe "restroom page" do 
+	  let(:restroom) { FactoryGirl.create(:restroom) }
+	  before { visit restroom_path(restroom) } 
+	
+	  it { should have_content(restroom.name) }
+	  it { should have_content(restroom.line1) } 
+	end
+  end 
 
-end
+end 
